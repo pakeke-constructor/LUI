5b58679403d52ee77e2b4d53bd8c0fd3d6b07951
Oli <OliverWDGarrett@gmail.com>
2024-02-13 18:01:02 +1300

    fixed scrollBox with removal of preferredSize stuff

--- elements/elements/TestBox.lua
+++ elements/elements/TestBox.lua
@@ -5,12 +5,12 @@ local TestBox = LUI.Element()
 local NUM_BUTTO = 10
 
 
-local function setPreferredHeight(self, x)
-    self.preferredHeight = x
+local function setHeight(self, x)
+    self.boxHeight = x
 end
 
 function TestBox:init()
-    setPreferredHeight(nil, 1000)
+    setHeight(self, 1000)
 
     self.buttons = {}
     for _=1, NUM_BUTTO do
@@ -21,14 +21,14 @@ function TestBox:init()
 end
 
 
-function TestBox:getPreferredHeight()
-    return self.preferredHeight
+function TestBox:getHeight()
+    return self.boxHeight
 end
 
 
 function TestBox:onRender(x,y,w,h)
     local r = Region(x,y,w,h):padPixels(20)
-    setPreferredHeight(self, 300 + 200 * math.sin(love.timer.getTime()/4))
+    setHeight(self, 300 + 200 * math.sin(love.timer.getTime()/4))
     love.graphics.setColor(1,0,0)
     love.graphics.setLineWidth(10)
     love.graphics.rectangle("line",r:get())
